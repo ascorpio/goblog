b7d849af769c4788107e68fd32c9919a33c3e4ff
wjh <ascorpio@qq.com>
2022-04-24 01:04:47 +0800

    自动重载

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 
 func handlerFunc(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path == "/" {
-		fmt.Fprint(w, "<h1>Hello, 这里是 goblog</h1>")
+		fmt.Fprint(w, "<h1>Hello, 欢迎来到 goblog！</h1>")
 	} else if r.URL.Path == "/about" {
 		fmt.Fprint(w, "此博客是用以记录编程笔记，如您有反馈或建议，请联系 "+
 			"<a href=\"mailto:ascorpio@qq.com\">ascorpio@qq.com</a>")
@@ -16,6 +16,7 @@ func handlerFunc(w http.ResponseWriter, r *http.Request) {
 			"<p>如有疑惑，请联系我们。</p>")
 	}
 }
+
 func main() {
 	http.HandleFunc("/", handlerFunc)
 	http.ListenAndServe(":3000", nil)
